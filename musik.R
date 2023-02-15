e1a3aa87da17fcfecd9ad9042b18e5448c4685df
mees707 <73827898+mees707@users.noreply.github.com>
2023-02-15 17:49:45 +0100

    Create musik.R

---
+++ musik.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(spotifyr) 
+ 
+juditha <- get_track_audio_features(c("2M5b9YLAgFroqWzeaZf86e", "3DBKc4ioGnMQLlbGQcFDIO"))
+
+alla <- get_album_tracks("7oI0E3DdTbD85rhMg19GSU")
+
+
+alla
